ecb1a7b6b9aa27eec3d646469e09d36f7ca79bd3
Tamal Kundu <tamalkundu2002@gmail.com>
2024-11-25 17:45:52 +0530

    hero fix

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -30,9 +30,11 @@ const HeroSection = () => {
               className="w-8 h-8 rounded-full"
             />
             <div className="text-slate-600 text-lg flex items-center gap-2 md:gap-5">
-              <p className="lg:text-lg text-xs">Guide by Ása Steinars</p>
-              <p className="lg:text-md text-xs">Norway</p>
-              <p className="lg:text-md text-xs">★ New</p>
+              <p className="lg:text-lg text-xs text-rexbygray-900">
+                Guide by Ása Steinars
+              </p>
+              <p className="lg:text-lg text-xs text-rexbygray-900">Norway</p>
+              <p className="lg:text-lg text-xs text-rexbygray-900">★ New</p>
             </div>
           </div>
           <p className="text-slate-600 text-md mt-4 leading-relaxed">
